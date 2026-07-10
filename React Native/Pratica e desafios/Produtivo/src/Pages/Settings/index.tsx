9e24d6274aa0372b3c0f1473dfef3da5d002cb94
mauriciolacerdan <mauriciolacerdan@gmail.com>
2026-07-10 16:06:39 -0300

    Transferindo Produtivo para repositorio proprio

--- React Native/Pratica e desafios/Produtivo/src/Pages/Settings/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, Texto } from './styles';
-
-export default function Settings() {
-  return (
-    <Container>
-      <Texto>Configs adicionar:excluir conta</Texto>
-    </Container>
-  );
-}
